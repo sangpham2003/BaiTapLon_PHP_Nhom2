4c2a1f90e5a3e975ef6a6cfd8154c2216af38088
sangpham2003 <145478732+sangpham2003@users.noreply.github.com>
2024-06-16 16:02:29 +0700

    Add files via upload

--- db_xemphim.sql
+++ db_xemphim.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2024 at 04:49 PM
+-- Generation Time: Jun 16, 2024 at 11:00 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_xemphim`
+-- Database: `db`
 --
 
 -- --------------------------------------------------------
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `phim` (
-  `MaPhim` varchar(15) NOT NULL,
+  `MaPhim` int(11) NOT NULL,
   `TenPhim` varchar(30) DEFAULT NULL,
   `TheLoai` varchar(255) DEFAULT NULL,
   `ThoiLuong` varchar(30) DEFAULT NULL,
@@ -45,8 +45,8 @@ CREATE TABLE `phim` (
 
 CREATE TABLE `phong` (
   `MaPhong` int(11) NOT NULL,
+  `MaRap` int(11) NOT NULL,
   `TenPhong` int(11) DEFAULT NULL,
-  `MaRap` varchar(255) NOT NULL,
   `SoLuongGhe` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -57,9 +57,23 @@ CREATE TABLE `phong` (
 --
 
 CREATE TABLE `rap` (
-  `MaRap` varchar(255) NOT NULL,
+  `MaRap` int(11) NOT NULL,
   `TenRap` varchar(30) DEFAULT NULL,
   `DiaChi` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suatchieu`
+--
+
+CREATE TABLE `suatchieu` (
+  `MaSuatChieu` int(11) NOT NULL,
+  `MaPhim` int(11) NOT NULL,
+  `MaPhong` int(11) NOT NULL,
+  `NgayChieu` datetime DEFAULT NULL,
+  `ThoiGianBatDau` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -69,11 +83,11 @@ CREATE TABLE `rap` (
 --
 
 CREATE TABLE `taikhoan` (
-  `MaTK` varchar(15) NOT NULL,
+  `MaTK` int(11) NOT NULL,
   `TenTK` varchar(30) NOT NULL,
   `MatKhau` varchar(30) NOT NULL,
   `DiaChi` varchar(30) DEFAULT NULL,
-  `NgaySinh` time(6) DEFAULT NULL,
+  `NgaySinh` date DEFAULT NULL,
   `Email` varchar(30) DEFAULT NULL,
   `SDT` varchar(15) DEFAULT NULL,
   `TenNguoiDung` varchar(30) NOT NULL
@@ -87,10 +101,10 @@ CREATE TABLE `taikhoan` (
 
 CREATE TABLE `vephim` (
   `MaVe` int(11) NOT NULL,
-  `PHIMMaPhim` varchar(15) NOT NULL,
-  `MaTK` varchar(15) NOT NULL,
-  `MaPhong` int(11) NOT NULL,
-  `LichChieu` int(11) DEFAULT NULL,
+  `MaSuatChieu` int(11) NOT NULL,
+  `MaTK` int(11) NOT NULL,
+  `GiaVe` int(11) DEFAULT NULL,
+  `PTTT` varchar(255) DEFAULT NULL,
   `ViTri` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -118,6 +132,14 @@ ALTER TABLE `rap`
   ADD PRIMARY KEY (`MaRap`);
 
 --
+-- Indexes for table `suatchieu`
+--
+ALTER TABLE `suatchieu`
+  ADD PRIMARY KEY (`MaSuatChieu`),
+  ADD KEY `FKSUATCHIEU191445` (`MaPhim`),
+  ADD KEY `FKSUATCHIEU717343` (`MaPhong`);
+
+--
 -- Indexes for table `taikhoan`
 --
 ALTER TABLE `taikhoan`
@@ -128,19 +150,42 @@ ALTER TABLE `taikhoan`
 --
 ALTER TABLE `vephim`
   ADD PRIMARY KEY (`MaVe`),
-  ADD KEY `FKVEPHIM327365` (`PHIMMaPhim`),
   ADD KEY `FKVEPHIM112359` (`MaTK`),
-  ADD KEY `FKVEPHIM696893` (`MaPhong`);
+  ADD KEY `FKVEPHIM734000` (`MaSuatChieu`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `phim`
+--
+ALTER TABLE `phim`
+  MODIFY `MaPhim` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `phong`
 --
 ALTER TABLE `phong`
   MODIFY `MaPhong` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rap`
+--
+ALTER TABLE `rap`
+  MODIFY `MaRap` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `suatchieu`
+--
+ALTER TABLE `suatchieu`
+  MODIFY `MaSuatChieu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  MODIFY `MaTK` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `vephim`
@@ -159,12 +204,18 @@ ALTER TABLE `phong`
   ADD CONSTRAINT `FKPHONG351691` FOREIGN KEY (`MaRap`) REFERENCES `rap` (`MaRap`);
 
 --
+-- Constraints for table `suatchieu`
+--
+ALTER TABLE `suatchieu`
+  ADD CONSTRAINT `FKSUATCHIEU191445` FOREIGN KEY (`MaPhim`) REFERENCES `phim` (`MaPhim`),
+  ADD CONSTRAINT `FKSUATCHIEU717343` FOREIGN KEY (`MaPhong`) REFERENCES `phong` (`MaPhong`);
+
+--
 -- Constraints for table `vephim`
 --
 ALTER TABLE `vephim`
   ADD CONSTRAINT `FKVEPHIM112359` FOREIGN KEY (`MaTK`) REFERENCES `taikhoan` (`MaTK`),
-  ADD CONSTRAINT `FKVEPHIM327365` FOREIGN KEY (`PHIMMaPhim`) REFERENCES `phim` (`MaPhim`),
-  ADD CONSTRAINT `FKVEPHIM696893` FOREIGN KEY (`MaPhong`) REFERENCES `phong` (`MaPhong`);
+  ADD CONSTRAINT `FKVEPHIM734000` FOREIGN KEY (`MaSuatChieu`) REFERENCES `suatchieu` (`MaSuatChieu`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
